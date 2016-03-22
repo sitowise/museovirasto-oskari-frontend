9ce47a9540bb582231b9bb869b31097f0a1b6580
unknown <Krzysztof Kozlowski@C-BYD-004.sitois.local>
2016-03-22 13:20:13 +0100

    add bundle for register search

---
+++ bundles/nba/nba-registers/instance.js
@@ -0,0 +1,50 @@
+﻿/**
+ * @class Oskari.nba.bundle.nba-registers.NbaRegistersBundleInstance
+ */
+Oskari.clazz.define('Oskari.nba.bundle.nba-registers.NbaRegistersBundleInstance',
+/**
+ * @method create called automatically on construction
+ * @static
+ */
+function () {
+    // Best practice is to initialize instance variables here.
+    //this.myVar = undefined;
+}, {
+    /**
+     * @static
+     * @property __name
+     */
+    __name: 'nba-registers',
+    /**
+     * Module protocol method
+     *
+     * @method getName
+     */
+    getName: function () {
+        return this.__name;
+    },
+    eventHandlers: {
+    },
+    /**
+     * DefaultExtension method for doing stuff after the bundle has started.
+     * 
+     * @method afterStart
+     */
+    afterStart: function (sandbox) {
+        //var conf = this.conf;
+
+        //Create the search service
+        this.searchService = Oskari.clazz.create('Oskari.nba.bundle.nba-registers.service.RegistersSearchService', this);
+
+        //Create the search tab
+        this.tab = Oskari.clazz.create('Oskari.nba.bundle.nba-registers.view.RegistersSearchTab', this);
+        this.tab.requestToAddTab();
+    },
+
+    getSearchService: function () {
+        return this.searchService;
+    }
+
+}, {
+    "extend": ["Oskari.userinterface.extension.DefaultExtension"]
+});
---
+++ bundles/nba/nba-registers/resources/locale/fi.js
@@ -0,0 +1,66 @@
+﻿Oskari.registerLocalization(
+{
+    "lang": "fi",
+    "key": "nba-registers",
+    "value": {
+        "title": "Registers",
+        "desc": "NBA Registers",
+        "tool": {
+            "tooltip": "NBA Registers"
+        },
+        "tab": {
+            "title": "Rekisterihaku",
+            "header": "Hae Museoviraston rekisterireiden kohteita",
+            "selectRegister": "Valitse rekisteri",
+            "allRegisters": "Kaikki rekisterit",
+            "limitToArea": "Rajaa haku kartalta valittuun alueeseen",
+            "search": "Hae",
+            "searchDesc": "Kirjoita rekisterikohteen nimi, nimen osa tai kohdetunnus",
+            "searchPlaceholder": "Kirjoita rekisterikohteen nimi, nimen osa tai kohdetunnus",
+            "showAllResultsOnMap": "Näytä kaikki tulokset kartalla",
+            "searchResults": "Hakutulokset",
+            "grid": {
+                "id": "Tunnus",
+                "desc": "Kuvaus",
+                "actions": "Toiminnot",
+                "editItems": "Muokkaa kohdetta",
+            }//,
+            //"searchSummary": "Hakutulokset: {0} hakutulosta haulle {1}"
+            //"errors": {
+            //}
+            /*
+            "searchTitle": "Search term",
+            "searchPlaceholder": "Search location by place name or address.",
+            "regionTitle": "Region filter",
+            "regionPlaceholder": "Filter search results by municipalities or administrative units.",
+            "additionalsTitle": "Search options",
+            "normalTitle": "Keyword search",
+            "normalDesc": "Search places by an exact term. Please note that the search is case-sensitive.",
+            "restrictedTitle": "Region filter",
+            "restrictedDesc": "Search places by drawing a region to the map.",
+            "fuzzyTitle": "Fuzzy search",
+            "fuzzyDesc": "Search places by an exact term and terms that are similar to that term.",
+            "exonymTitle": "Exonym mode",
+            "exonymDesc": "Placenames in other languages are included in the search results.",
+            "searchButton": "Search",
+            "resultsTitle": "Search results",
+            "fuzzyResultsTitle": "Fuzzy search results",
+            "filterResultsTitle": "Region filter results",
+            "countryFilter": "Select country",
+            "backButton": "Back",
+            "geolocButton": "Locate nearest place",
+            "geolocDesc": "Click the map and locate the placename nearest the place.",
+            "noResults": "No search results",
+            "errors": {
+                "searchTermMissing": "An error occurred. The search term is required. Please type it and try again.",
+                "searchFailed": "An error occurred. The search could not be completed."
+            },
+            "grid": {
+                "name": "Name",
+                "village": "Region",
+                "type": "Type"
+            }*/
+        }
+    }
+}
+);
