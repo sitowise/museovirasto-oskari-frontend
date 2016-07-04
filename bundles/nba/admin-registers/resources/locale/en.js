331dd27dc8999b057fb6ecb253006cc0411b0345
Jaakko Ruutiainen <Jaakko.Ruutiainen@sito.fi>
2016-07-04 17:23:44 +0300

    Add new bundle for administering registry permissions

---
+++ bundles/nba/admin-registers/resources/locale/en.js
@@ -0,0 +1,32 @@
+Oskari.registerLocalization(
+{
+    "lang": "en",
+    "key": "admin-registers",
+    "value": {
+        "title": "Register Rights",
+        "desc": "NOT TRANSLATED",
+        "tile": {
+            "title": "Register Rights"
+        },
+        "flyout": {
+            "title": "Register Rights",
+            "message": "Register Rights Management"
+        },
+        "rights": {
+            "name": "Register",
+            "rightToPublish": "Publish",
+            "success": {
+                "title": "Register Rights Saved",
+                "message": "Register rights have been updated."
+            },
+            "error": {
+                "title": "Error",
+                "message": "Register rights could not been updated."
+            },
+            "selectValue": "Select value"
+        },
+        "save": "Save",
+        "selectRole": "Role"
+    }
+}
+);
