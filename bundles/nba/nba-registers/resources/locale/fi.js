76b69fa8e34e18d58ea328686fd57d9ca1a46db9
Krzysztof Kozlowski <krzysztof.kozlowski@sito.fi>
2016-10-21 15:26:19 +0200

    [MUSEO-123] add search of resource registry items

--- bundles/nba/nba-registers/resources/locale/fi.js
+++ bundles/nba/nba-registers/resources/locale/fi.js
@@ -34,43 +34,10 @@
                 "RKY1993": "RKY1993",
                 "RKY2000": "RKY2000",
                 "WorldHeritageItem": "Maailmanperintökohteet",
-                "ProjectItem": "Project"
+                "ProjectItem": "Project",
+                "HistoricalMunicipality": "Resurssi",
+                "KYSItem": "Resurssi"
             }
-            //"searchSummary": "Hakutulokset: {0} hakutulosta haulle {1}"
-            //"errors": {
-            //}
-            /*
-            "searchTitle": "Search term",
-            "searchPlaceholder": "Search location by place name or address.",
-            "regionTitle": "Region filter",
-            "regionPlaceholder": "Filter search results by municipalities or administrative units.",
-            "additionalsTitle": "Search options",
-            "normalTitle": "Keyword search",
-            "normalDesc": "Search places by an exact term. Please note that the search is case-sensitive.",
-            "restrictedTitle": "Region filter",
-            "restrictedDesc": "Search places by drawing a region to the map.",
-            "fuzzyTitle": "Fuzzy search",
-            "fuzzyDesc": "Search places by an exact term and terms that are similar to that term.",
-            "exonymTitle": "Exonym mode",
-            "exonymDesc": "Placenames in other languages are included in the search results.",
-            "searchButton": "Search",
-            "resultsTitle": "Search results",
-            "fuzzyResultsTitle": "Fuzzy search results",
-            "filterResultsTitle": "Region filter results",
-            "countryFilter": "Select country",
-            "backButton": "Back",
-            "geolocButton": "Locate nearest place",
-            "geolocDesc": "Click the map and locate the placename nearest the place.",
-            "noResults": "No search results",
-            "errors": {
-                "searchTermMissing": "An error occurred. The search term is required. Please type it and try again.",
-                "searchFailed": "An error occurred. The search could not be completed."
-            },
-            "grid": {
-                "name": "Name",
-                "village": "Region",
-                "type": "Type"
-            }*/
         }
     }
 }
