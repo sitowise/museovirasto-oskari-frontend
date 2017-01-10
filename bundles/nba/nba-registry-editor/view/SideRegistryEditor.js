1cf2c1ccf86f293560ee55377daacd183421a9d1
Jaakko Ruutiainen <jaakko.ruutiainen@sito.fi>
2017-01-10 12:47:31 +0200

    Set style to current to force redraw

--- bundles/nba/nba-registry-editor/view/SideRegistryEditor.js
+++ bundles/nba/nba-registry-editor/view/SideRegistryEditor.js
@@ -666,11 +666,14 @@ Oskari.clazz.define('Oskari.nba.bundle.nba-registry-editor.view.SideRegistryEdit
             var me = this,
                 wfsLayerPlugin = me.sandbox.findRegisteredModuleInstance('MainMapModuleWfsLayerPlugin'),
                 layerService = me.sandbox.getService('Oskari.mapframework.service.MapLayerService'),
-                mapModule = this.sandbox.findRegisteredModuleInstance('MainMapModule');
+                mapModule = me.sandbox.findRegisteredModuleInstance('MainMapModule');
             
             $.each(me.itemData.mapLayers, function(i, obj) {
                 var layer = layerService.findMapLayer(obj.mapLayerID);
                 wfsLayerPlugin.deleteTileCache(obj.mapLayerID, layer.getCurrentStyle().getName());
+                
+                var evt = me.sandbox.getEventBuilder('AfterChangeMapLayerStyleEvent')(layer);
+                me.sandbox.notifyAll(evt);
             });
             
             //force refresh of layers
