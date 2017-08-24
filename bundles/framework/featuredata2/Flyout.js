56b3dda99a8346206382faf142c3b9b43afb4c20
Jaakko Ruutiainen <jaakko.ruutiainen@sito.fi>
2017-08-24 15:42:55 +0300

    Add zoom to link only when there is some data

--- bundles/framework/featuredata2/Flyout.js
+++ bundles/framework/featuredata2/Flyout.js
@@ -482,7 +482,9 @@ Oskari.clazz.define(
                 me._addFeatureValues(model, fields, hiddenFields, selectedFeatures, null);
 
                 fields = model.getFields();
-                fields.unshift(me.instance.localization.zoomTo);
+                if(!!fields && fields.length > 0) {
+                    fields.unshift(me.instance.localization.zoomTo);
+                }
                 hiddenFields.push('__fid');
                 hiddenFields.push('__centerX');
                 hiddenFields.push('__centerY');
@@ -574,18 +576,18 @@ Oskari.clazz.define(
                 _.forEach(visibleFields, function (field) {
                     grid.setNumericField(field, me._fixedDecimalCount);
                 });
-                if(!!visibleFields) {
-                    panel.grid.setColumnValueRenderer(visibleFields[0], function(value, rowData) {
-                        var zoomToLink = me.templateLink.clone();
-                        zoomToLink.append(me.instance.localization.zoomTo);
-                        zoomToLink.click(function(event) {
-                            var moveReqBuilder = me.instance.sandbox.getRequestBuilder('MapMoveRequest');
-                            var moveReq = moveReqBuilder(rowData.__centerX, rowData.__centerY, { scale : 1000 });
-                            me.instance.sandbox.request(me.instance, moveReq);
-                        });
-                        return zoomToLink;
+                panel.grid.setColumnValueRenderer(me.instance.localization.zoomTo, function(value, rowData) {
+                    var zoomToLink = me.templateLink.clone();
+                    zoomToLink.append(me.instance.localization.zoomTo);
+                    zoomToLink.click(function(event) {
+                        var moveReqBuilder = me.instance.sandbox.getRequestBuilder('MapMoveRequest');
+                        var moveReq = moveReqBuilder(rowData.__centerX, rowData.__centerY, { scale : 1000 });
+                        me.instance.sandbox.request(me.instance, moveReq);
                     });
-                }
+                    if(!!rowData.__centerX && !!rowData.__centerY) {
+                        return zoomToLink;
+                    }
+                });
                 panel.grid.renderTo(panel.getContainer());
                 // define flyout size to adjust correctly to arbitrary tables
                 var mapdiv = this.instance.sandbox.findRegisteredModuleInstance('MainMapModule').getMapEl(),
