553bd56581951891203247c4969d9a397dd285bc
Jaakko Ruutiainen <jaakko.ruutiainen@sito.fi>
2017-08-24 08:16:58 +0300

    Fix numerical ordering when order numbers have different amount of digits

--- bundles/framework/layerselector2/Flyout.js
+++ bundles/framework/layerselector2/Flyout.js
@@ -418,16 +418,25 @@ Oskari.clazz.define('Oskari.mapframework.bundle.layerselector2.Flyout',
                 nameB = b[groupingMethod]().toLowerCase();
             if (nameA === nameB && (a.getName() && b.getName())) {
                 if(groupingMethod === 'getInspireName' && a.getLayerOrderNumber() && (a.getLayerOrderNumber() !== b.getLayerOrderNumber())) {
-                    nameA = ""+a.getLayerOrderNumber();
+                    nameA = a.getLayerOrderNumber();
                 } else {
                     nameA = a.getName().toLowerCase();
                 }
 
                 if(groupingMethod === 'getInspireName' && b.getLayerOrderNumber() && (a.getLayerOrderNumber() !== b.getLayerOrderNumber())) {
-                    nameB = ""+b.getLayerOrderNumber();
+                    nameB = b.getLayerOrderNumber();
                 } else {
                     nameB = b.getName().toLowerCase();
                 }
+            }
+            if($.isNumeric(nameA) && $.isNumeric(nameB)) {
+                return nameA - nameB;
+            }            
+            if($.isNumeric(nameA)) {
+                return -1;
+            }            
+            if($.isNumeric(nameB)) {
+                return 1;
             }
             if (nameA < nameB) {
                 return -1;
